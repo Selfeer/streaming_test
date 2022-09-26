d372349e7430939d2bd4076ac18e6cce053ecc15
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2022-09-26 22:05:52 +0400

    hhi

--- Tests/test_streaming.py
+++ Tests/test_streaming.py
@@ -1,15 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import pytest
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-driver = webdriver.Chrome(service=Service('chromedriver.exe'))
-driver.get("https://www.youtube.com/watch?v=MItluIsvfTo&ab_channel=AmazonWebServices")
-options = webdriver.ChromeOptions()
-options.add_argument("--use-file-for-fake-video-capture=C:\\Users\\selfe\\PycharmProjects\\streaming_test\\test_video"
-                     ".m4v")
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("use-fake-ui-for-media-stream")
+chrome_options.add_argument("--use-file-for-fake-video-capture=C:\\Users\\Selfeer\\PycharmProjects\\streaming_test"
+                            "\\test_video.m4v")
+
+driver = webdriver.Chrome(service=Service('chromedriver.exe'), chrome_options=chrome_options)
+
+driver.get("https://webcamtests.com/")
+wait = WebDriverWait(driver, 20)
+
+start_streaming = wait.until(
+        EC.presence_of_element_located((By.ID, "webcam-launcher"))
+    ).click()
+
 sleep(5)
 # driver.find_element(By.ID, 'userEmail').send_keys("david_test@gmail.com")
 
